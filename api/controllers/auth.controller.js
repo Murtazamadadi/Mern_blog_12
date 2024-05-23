88a42d3509e983b5fb934d3a7b8c0e37c6d3b36e
MutazaMadadi <murtazamadadi12@gmail.com>
2024-05-23 12:08:30 +0330

    create middleware and function for error handling

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,14 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
+import { errorHandler } from "../utils/error.js"
 
-export const signUp= async(req,res)=>{
+
+export const signUp= async(req,res,next)=>{
     const {username,email,password}=req.body
 
     if(!username || !email || !password || username==="" || email==="" || password===""){
-        res.json("وارید کردن تمام اطلاعات الزامی می باشد")
+        // res.json("وارید کردن تمام اطلاعات الزامی می باشد")
+        next(errorHandler(400,"وارید کردن همه اطلاعات الزامی می باشد"))
     }
 
     const hashpassword=bcryptjs.hashSync(password,10)
@@ -20,6 +23,7 @@ export const signUp= async(req,res)=>{
         await newUser.save()
         res.status(200).json(newUser)
     }catch(err){
-        res.status(400).json(err.message)
+        // res.status(400).json(err.message)
+        next(err)
     }
 }
--- api/index.js
+++ api/index.js
@@ -28,3 +28,15 @@ app.listen(3000,()=>{
 
 // app.use("/api",userRouter)
 app.use("/api/auth",authRouter)
+
+
+// =========================================Middleware
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || "internal error";
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    })
+})
