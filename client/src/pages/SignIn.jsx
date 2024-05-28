8f1b3f1280113b58e729617fd37dd196edf73c5e
MutazaMadadi <murtazamadadi12@gmail.com>
2024-05-28 10:26:22 +0330

    complete google OAuth functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -5,6 +5,7 @@ import { Link,useNavigate } from 'react-router-dom';
 
 import { signInStart,signInSuccess,signInFailure } from '../redux/user/userSlice';
 import { useDispatch,useSelector } from 'react-redux';
+import OAuth from '../components/OAuth';
 
 export default function SignUp() {
   const dispatch=useDispatch()
@@ -107,6 +108,7 @@ export default function SignUp() {
               )}
            
             </Button>
+            <OAuth/>
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>آیاثبت نام کرده اید؟</span>
