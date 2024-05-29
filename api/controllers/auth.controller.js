7fa0c97d4d041d8d9ad63ec8b833e2f2ab7f50ac
MutazaMadadi <murtazamadadi12@gmail.com>
2024-05-29 09:41:51 +0330

    complete Google auth UI and api both

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -59,3 +59,39 @@ export const signIn=async(req,res,next)=>{
         next(err)
     }
 }
+
+// ================================================== Google OAuth
+
+export const google=async(req,res,next)=>{
+    const {name,email,googlePhotoUrl}=req.body
+
+    try{
+        const user=await User.findOne({email})
+        if(user){
+            const token=jwt.sign({id:user._id},process.env.JWT_SECRET)
+            
+            const {password:pase,...rest}=user._doc
+
+            res.status(200).cookie("access_token",token,{httpOnly:true}).json(rest)
+        }else{
+            const generatePassword=Math.random().toString(36).slice(-8)+Math.random().toString(36).slice(-8)
+            const hashGeneratePassword=bcryptjs.hashSync(generatePassword,10)
+
+            const newUser=new User({
+                username:name.toLowerCase().split(" ").join("").Math.random().toString(9).slice(-4),
+                password:hashGeneratePassword,
+                email,
+                profileImage:googlePhotoUrl
+            })
+
+            const token=jwt.sign({id:newUser._id},process.env.JWT_SECRET)
+
+            const {password: pass,...rest}=newUser._doc
+
+            res.status(200).cookie("access_token",token,{httpOnly:true}).json(rest)
+        }
+    }catch(err){
+        console.log(err)
+        next(err)
+    }
+}
